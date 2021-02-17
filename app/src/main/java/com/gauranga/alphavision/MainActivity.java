6a86efa7a5d36a2bf1d5f6e6e995ff8a7e2abbee
Gauranga Das <dasgauranga2@gmail.com>
2021-02-17 18:54:10 +0530

    New Directory Activity basic layout design

--- app/src/main/java/com/gauranga/alphavision/MainActivity.java
+++ app/src/main/java/com/gauranga/alphavision/MainActivity.java
@@ -2,9 +2,17 @@ package com.gauranga.alphavision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // add a new image directory
+    public void new_directory(View view) {
+        Intent intent = new Intent(getApplicationContext(),NewDirectoryActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
