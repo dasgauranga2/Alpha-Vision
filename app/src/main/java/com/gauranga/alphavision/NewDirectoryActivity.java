ae065c4121775784a2dae61f8e489fd3fb1893bf
Gauranga Das <dasgauranga2@gmail.com>
2021-02-17 20:07:33 +0530

    Launch gallery and retrieve images

--- app/src/main/java/com/gauranga/alphavision/NewDirectoryActivity.java
+++ app/src/main/java/com/gauranga/alphavision/NewDirectoryActivity.java
@@ -1,14 +1,65 @@
 package com.gauranga.alphavision;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class NewDirectoryActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE = 100;
+    ImageView image;
+    List<Uri> image_uris;
+
+    // add new image to the directory
+    public void add_image_gallery(View view) {
+        // create an intent to launch the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the image is successfully retrieved
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            // get the image in uri format
+            Uri image_uri = data.getData();
+            // add the image uri to the list
+            image_uris.add(image_uri);
+            // setup the recycler view
+            setup_recyclerview();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_directory);
+
+        image = findViewById(R.id.imageView);
+        image_uris = new LinkedList<>();
+        Toast.makeText(getApplicationContext(), "HELLO", Toast.LENGTH_SHORT).show();
+    }
+
+    // setup the recycler view
+    // containing a list of images
+    public void setup_recyclerview() {
+        RecyclerView recyclerView = findViewById(R.id.imageRecyclerView);
+        NewDirectoryAdapter newDirectoryAdapter = new NewDirectoryAdapter(this,image_uris);
+        recyclerView.setAdapter(newDirectoryAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/gauranga/alphavision/NewDirectoryAdapter.java
@@ -0,0 +1,60 @@
+package com.gauranga.alphavision;
+
+//public class NewDirectoryAdapter {
+//}
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NewDirectoryAdapter extends RecyclerView.Adapter<NewDirectoryAdapter.MyViewHolder> {
+
+    List<Uri> image_uris;
+    Context context;
+
+    // the context and the data is passed to the adapter
+    public NewDirectoryAdapter(Context ct, List<Uri> img_uris) {
+        context = ct;
+        image_uris = img_uris;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        // The 'new_directory_row.xml' file in layout folder defines
+        // the style for each row
+        View view = inflater.inflate(R.layout.new_directory_row,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.image.setImageURI(image_uris.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        // return the length of the recycler view
+        return image_uris.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.currentDirImage);
+        }
+    }
+}
