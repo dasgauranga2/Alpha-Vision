c23e4a14a795b1edadd72177b096407e336cf1f3
Gauranga Das <dasgauranga2@gmail.com>
2021-02-18 11:49:47 +0530

    Create new directory where images will be stored

--- app/src/main/java/com/gauranga/alphavision/MainActivity.java
+++ app/src/main/java/com/gauranga/alphavision/MainActivity.java
@@ -2,9 +2,14 @@ package com.gauranga.alphavision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ContextWrapper wrapper = new ContextWrapper(getApplicationContext());
+        // root directory where all the data is stored
+        File root_dir = wrapper.getDir("IMAGES5", MODE_PRIVATE);
+        // array of all the sub-directories
+        File[] files = root_dir.listFiles();
+
+        for (File f : files) {
+            // array of images files in each sub-directory
+            File[] image_files = f.listFiles();
+            for (File image : image_files) {
+                Log.i("FILE_INFO", f.getName() + " " + image.getName());
+            }
+        }
     }
 }
--- app/src/main/java/com/gauranga/alphavision/NewDirectoryActivity.java
+++ app/src/main/java/com/gauranga/alphavision/NewDirectoryActivity.java
@@ -5,22 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.LinkedList;
 import java.util.List;
 
 public class NewDirectoryActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 100;
-    ImageView image;
     List<Uri> image_uris;
+    EditText directory_name;
 
     // add new image to the directory
     public void add_image_gallery(View view) {
@@ -28,6 +36,35 @@ public class NewDirectoryActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, PICK_IMAGE);
+    }
+
+    // create a new directory
+    public void create_directory(View view) throws IOException {
+        // get the name of the directory we want to create
+        String name = directory_name.getText().toString();
+
+        ContextWrapper wrapper = new ContextWrapper(getApplicationContext());
+        // root directory where all data is stored
+        File root_dir = wrapper.getDir("IMAGES5", MODE_PRIVATE);
+        // sub-directory we want to create
+        File new_dir = new File(root_dir, name);
+        // create the sub-directory
+        new_dir.mkdir();
+        for (Uri image_uri : image_uris) {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image_uri);
+            // create an image file in the sub-directory
+            File image_file = new File(new_dir, System.currentTimeMillis() + ".jpg");
+            OutputStream stream = null;
+            // an output stream that writes bytes to a file
+            stream = new FileOutputStream(image_file);
+            // write a compressed version of the bitmap to the specified output stream
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+            stream.flush();
+            stream.close();
+        }
+
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -49,9 +86,8 @@ public class NewDirectoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_directory);
 
-        image = findViewById(R.id.imageView);
         image_uris = new LinkedList<>();
-        Toast.makeText(getApplicationContext(), "HELLO", Toast.LENGTH_SHORT).show();
+        directory_name = findViewById(R.id.directoryNameText);
     }
 
     // setup the recycler view
